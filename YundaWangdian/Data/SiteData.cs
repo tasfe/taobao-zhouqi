5bad183d89e0543532b346f93344c08bbab8dd1d
continue88@gmail.com <continue88@gmail.com@9c6b7047-04d1-d58b-14d8-e57a51941fd5>
2012-12-12 18:16:37 +0000

    update

--- YundaWangdian/Data/SiteData.cs
+++ YundaWangdian/Data/SiteData.cs
@@ -16,5 +16,38 @@ namespace YundaWangdian.Data
 
         [XmlAttribute("City")]
         public string City { get; set; }
+
+        [XmlAttribute("psfw")]
+        public string psfw { get; set; }
+
+        [XmlAttribute("bz")]
+        public string bz { get; set; }
+
+        [XmlAttribute("dz")]
+        public string dz { get; set; }
+
+        [XmlAttribute("bpsfw")]
+        public string bpsfw { get; set; }
+
+        [XmlAttribute("jj")]
+        public string jj { get; set; }
+
+        [XmlAttribute("mc")]
+        public string mc { get; set; }
+
+        [XmlAttribute("dh")]
+        public string dh { get; set; }
+
+        [XmlAttribute("sjgs")]
+        public string sjgs { get; set; }
+
+        [XmlAttribute("szd")]
+        public string szd { get; set; }
+
+        [XmlAttribute("bm")]
+        public string bm { get; set; }
+
+        [XmlAttribute("fzr")]
+        public string fzr { get; set; }
     }
 }
